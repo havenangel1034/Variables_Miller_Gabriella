69d6ef4fa3fbdef86bdd7685e1496ec47e0d756b
Gabby <gabbie10112005@gmail.com>
2024-11-15 09:24:11 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Variables_Miller_Gabriella
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x; //declaration
+            string playerName; //string declaration
+            bool isRunning;
+
+            float grade = 99.99f; //initization
+            bool isGrounded = true; //initizing isGrounded to true
+
+
+            x = 6;
+            Console.WriteLine(x);
+            Console.WriteLine("What is your name, Player?");
+            playerName = Console.ReadLine();
+            Console.WriteLine(playerName);
+
+        }
+    }
+        
+}
